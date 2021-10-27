6e7b2719d6158001e5d61fcc10aca0c52a82114d
janessapong <janessapong@gmail.com>
2021-10-27 12:36:52 -0700

    added main.cpp

---
+++ main.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+//  testing
+//
+//  Created by Janessa Pong on 2021-10-25.
+//
+
+#include <iostream>
+#include <stdio.h>
+
+void shenanigans(int num){
+    for(int i=0; i < num; i++){
+        std::cout << "dick" << std::endl;
+    }
+    return;
+}
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    
+    shenanigans(5);
+    
+    return 0;
+    
+    
+}
+
+
